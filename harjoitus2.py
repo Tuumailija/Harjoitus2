ff3475f1f7b6dbdc549809d6272ae805f75fd5f2
juhay-git <juha.yli-hemminki@seamk.fi>
2024-09-27 10:25:29 +0300

    Lisää tulostusta

--- harjoitus2.py
+++ harjoitus2.py
@@ -1,2 +1,12 @@
 #Markus Koski
 print("Hello World")
+
+def hello(n):
+    if n == 1:
+        print("!!!")
+        return 1
+    else:
+        print("Hello", end=" ")
+        return hello(n - 1)
+
+hello(4)
